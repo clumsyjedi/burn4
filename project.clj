a9168202b868485abca3ac4f9142e749c83a5ff2
Frazer Irving <frazer.irving@mailonline.com>
2018-01-28 22:44:41 +1100

    fml

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject burn3 "0.1.0-SNAPSHOT"
+(defproject burn4 "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
@@ -8,8 +8,8 @@
                  [stencil/stencil "0.5.0"]
                  [ring/ring-jetty-adapter  "1.2.1"] ]
   :plugins [[lein-ring "0.9.7"]]
-  :ring {:handler burn3.handler/app}
-  :main burn3.handler
+  :ring {:handler burn4.handler/app}
+  :main burn4.handler
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring/ring-mock "0.3.0"]]}})
